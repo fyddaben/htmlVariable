1315344ab0a9adb51c9ed41fee434b400f3bb73f
fyddaben <838730593@qq.com>
2015-12-31 16:55:38 +0000

    update semantic

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
 var htmlparser = require("front-htmlparser2");
+
 var request = require('request');
+
 var log4js = require('log4js');
+
+var cheerio = require('cheerio');
+
 log4js.configure('hv_log4js_configuration.json', { cwd: __dirname+'/log/'});
+
 var logger = log4js.getLogger('hv_log');
-var option = {
-  onopentag: function(name, attribs){
-    if(name === "script" && attribs.type === "text/javascript"){
-      logger.info("JS! Hooray!");
+
+function analyseUrl(url) {
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(body, {decodeEntities: false});
+      var text = $('body').html();
+      var dom = htmlparser.parseDOM(text);
+      loopCheck(dom, 0);
+      logger.info(JSON.stringify(treeData));
     }
-  },
-  oncomment: function(data) {
-    console.log(data);
-  },
-  ontext: function(text){
-    text = text.trim();
-    if(text) {
-      logger.info("-->", text);
+  })
+}
+var treeData = [];
+function loopCheck(dom, level) {
+  for(var i in dom) {
+    var d = dom[i];
+    if(d.type=='script') {
+      continue;
     }
-  },
-  onclosetag: function(tagname){
-    if(tagname === "script"){
-      logger.info("That's it?!");
+    if(d.type=='text'){
+      if (d.data.trim()) {
+        var obj = {
+          level: level,
+          text: d.data
+        }
+        treeData.push(obj);
+      }
+    }
+    if (d.children) {
+      var le = level + 1;
+      loopCheck(d.children, le);
     }
   }
-};
+}
 
-var parser = new htmlparser.Parser(option, {decodeEntities: true});
+//analyseUrl('http://www.mi.com/sg/events/christmas2015/sale/');
 
-request('http://hd.mi.com/z/12161a/index.html', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    parser.write(body);
-    parser.end();
-  }
-})
-//parser.write("Xyz <script type='text/javascript'>var foo = '<<bar>>';</ script>");
-//parser.end();
-//var dom = htmlparser.parseDOM("Xyz <script type='text/javascript'>var foo = '<<bar>>';</ script>asdfadf");
-//console.log(dom);
+analyseUrl('http://hd.mi.com/z/12161a/index.html');
 
 
